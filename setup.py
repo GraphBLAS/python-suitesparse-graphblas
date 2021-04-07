a23b83dd6c29511d2bb760e0d7472365bedd9604
Michel Pelletier <pelletier.michel@gmail.com>
2021-04-07 12:50:57 -0700

    the last hand built suitesparse header and s imple cffi build script from grblas.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='suitesparse-graphblas',
+    version='4.0.3',
+    description='SuiteSparse:GraphBLAS Python bindings.',
+    packages=find_packages(),
+    author='Michel Pelletier, James Kitchen, Erik Welch',
+    cffi_modules=["suitesparse/graphblas/build.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0"],
+)
+
