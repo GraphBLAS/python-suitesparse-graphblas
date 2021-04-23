009d3d689d4375b3ff865c7ee10adfaebd7bf529
Erik Welch <erik.n.welch@gmail.com>
2021-04-22 20:52:37 -0500

    Basic `initialize(...)` and `is_initialized`

--- suitesparse_graphblas/__init__.py
+++ suitesparse_graphblas/__init__.py
@@ -1,5 +1,45 @@
 from ._graphblas import ffi, lib  # noqa
+from . import utils
 from ._version import get_versions
+
+is_initialized = False
+
+
+def initialize(*, blocking=False, memory_manager="numpy"):
+    """Initialize GraphBLAS via GrB_init or GxB_init.
+
+    This must be called before any other GraphBLAS functions are called.
+    A RuntimeError will be raised if called more than once.
+
+    Parameters
+    ----------
+    blocking : bool, optional
+        Whether to call init with GrB_BLOCKING or GrB_NONBLOCKING.
+        Default is False.
+    memory_manager : {'numpy', 'c'}, optional
+        Choose which malloc/free functions to use.  'numpy' uses numpy's
+        allocators, which makes it safe to perform zero-copy to and from numpy,
+        and allows Python to track memory usage via tracemalloc (if enabled).
+        'c' uses the default allocators.  Default is 'numpy'.
+
+    The global variable `suitesparse_graphblas.is_initialized` indicates whether
+    GraphBLAS has been initialized.
+    """
+    global is_initialized
+    if is_initialized:
+        raise RuntimeError(
+            "suitesparse-python is already initialized!  Unable to initialize again."
+        )
+    blocking = lib.GrB_BLOCKING if blocking else lib.GrB_NONBLOCKING
+    memory_manager = memory_manager.lower()
+    if memory_manager == "numpy":
+        utils.call_gxb_init(ffi, lib, blocking)
+    elif memory_manager == "c":
+        lib.GrB_init(blocking)
+    else:
+        raise ValueError(f'memory_manager argument must be "numpy" or "c"; got: {memory_manager!r}')
+    is_initialized = True
+
 
 __version__ = get_versions()["version"]
 del get_versions
