128ae68ec834d8dbb7e772299f02ae2df468ce35
Erik Welch <erik.n.welch@gmail.com>
2021-04-26 17:49:38 -0500

    Include "Library/include" in include path on Windows

--- suitesparse_graphblas/build.py
+++ suitesparse_graphblas/build.py
@@ -10,11 +10,15 @@ ffibuilder = FFI()
 with open(os.path.join(thisdir, "source.c")) as f:
     source = f.read()
 
+include_dirs = [os.path.join(sys.prefix, "include")]
+if sys.platform == "win32":
+    include_dirs.append(os.path.join(sys.prefix, "Library", "include"))
+
 ffibuilder.set_source(
     "suitesparse_graphblas._graphblas",
     source,
     libraries=["graphblas"],
-    include_dirs=[os.path.join(sys.prefix, "include")],
+    include_dirs=include_dirs,
 )
 
 header = "suitesparse_graphblas.h"
