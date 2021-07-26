f1241ee95e45a87eb47d31c3046778cde0420528
Michel Pelletier <pelletier.michel@gmail.com>
2021-07-26 16:40:18 -0400

    test wheels

--- suitesparse.sh
+++ suitesparse.sh
@@ -1,6 +1,9 @@
-VERSION=$1
-a=( ${version//./ } )
-VERSION="${a[0]}.${a[1]}.${a[2]}"
+
+if [[ $1 =~ refs/tags/([0-9]\.[0-9]\.[0-9]).*$ ]];
+then
+    VERSION=${BASH_REMATCH[1]}
+else
+    exit -1
 
 curl -L https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v${VERSION}.tar.gz | tar xzf -
 cd GraphBLAS-${VERSION}/build
