ea23aa32491547f7e31a019345fb76ec38665dd3
Michel Pelletier <pelletier.michel@gmail.com>
2021-07-26 14:09:49 -0400

    test wheels

--- suitesparse.sh
+++ suitesparse.sh
@@ -1,3 +1,5 @@
+echo "REf!"
+echo $GITHUB_REF
 curl -Ls https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v5.1.5.tar.gz | tar xzf -
 cd GraphBLAS-5.1.5/build
 cmake .. -DGBCOMPACT=1
