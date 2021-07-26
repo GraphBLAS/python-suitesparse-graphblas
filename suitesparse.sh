89f1f6351e0bd1adf62dc751894f18bdc1affb08
Michel Pelletier <michelp@users.noreply.github.com>
2021-07-26 18:21:44 -0400

    Github Action to build and upload linux wheels (#20)
    * test wheels
    
    * review comments from Erik

---
+++ suitesparse.sh
@@ -0,0 +1,13 @@
+
+if [[ $1 =~ refs/tags/v([0-9]\.[0-9]\.[0-9]).*$ ]];
+then
+    VERSION=${BASH_REMATCH[1]}
+else
+    exit -1
+fi
+
+curl -L https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v${VERSION}.tar.gz | tar xzf -
+cd GraphBLAS-${VERSION}/build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+make install
