3ab4f6ed06a06eefc3798e48450b9d122738eba1
Michel Pelletier <pelletier.michel@gmail.com>
2021-07-26 14:20:44 -0400

    test wheels

--- suitesparse.sh
+++ suitesparse.sh
@@ -2,7 +2,7 @@ VERSION=${GITHUB_REF}
 a=( ${version//./ } )
 VERSION="${a[0]}.${a[1]}.${a[2]}"
 
-curl -Ls https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v${VERSION}.tar.gz | tar xzf -
+curl -L https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v${VERSION}.tar.gz | tar xzf -
 cd GraphBLAS-${VERSION}/build
 cmake .. -DGBCOMPACT=1
 make -j$(nproc)
