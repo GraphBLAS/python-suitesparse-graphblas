17cbd452c9703c15cac6aab79a36ec3b3dfd9ea5
Michel Pelletier <pelletier.michel@gmail.com>
2021-07-26 16:45:06 -0400

    test wheels

--- suitesparse.sh
+++ suitesparse.sh
@@ -4,6 +4,7 @@ then
     VERSION=${BASH_REMATCH[1]}
 else
     exit -1
+fi
 
 curl -L https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v${VERSION}.tar.gz | tar xzf -
 cd GraphBLAS-${VERSION}/build
