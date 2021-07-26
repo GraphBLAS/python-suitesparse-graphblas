dfc842287791f9aeee575f04d2df2fd3827537c6
Michel Pelletier <pelletier.michel@gmail.com>
2021-07-26 16:21:59 -0400

    test wheels

--- suitesparse.sh
+++ suitesparse.sh
@@ -1,8 +1,6 @@
-# VERSION=${GITHUB_REF}
-# a=( ${version//./ } )
-# VERSION="${a[0]}.${a[1]}.${a[2]}"
-
-VERSION='5.1.5'
+VERSION=$1
+a=( ${version//./ } )
+VERSION="${a[0]}.${a[1]}.${a[2]}"
 
 curl -L https://github.com/DrTimothyAldenDavis/GraphBLAS/archive/refs/tags/v${VERSION}.tar.gz | tar xzf -
 cd GraphBLAS-${VERSION}/build
