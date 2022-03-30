e1658be1ed4cd640ad8678f3d06406c3a6168a20
Erik Welch <erik.n.welch@gmail.com>
2022-03-30 18:05:26 -0500

    Update Cython code to support UDTs

--- suitesparse_graphblas/utils.pyx
+++ suitesparse_graphblas/utils.pyx
@@ -5,7 +5,6 @@ from numpy cimport (
     NPY_ARRAY_OWNDATA,
     NPY_ARRAY_WRITEABLE,
     PyArray_New,
-    PyArray_SimpleNewFromData,
     import_array,
     ndarray,
     npy_intp,
@@ -34,8 +33,13 @@ cpdef ndarray claim_buffer(ffi, cdata, size_t size, dtype):
     cdef:
         npy_intp dims = size
         uintptr_t ptr = int(ffi.cast("uintptr_t", cdata))
-        ndarray array = PyArray_SimpleNewFromData(1, &dims, dtype.num, <void*>ptr)
+        ndarray array = PyArray_New(
+            ndarray, 1, &dims, dtype.num, NULL, <void*>ptr, dtype.itemsize,
+            NPY_ARRAY_WRITEABLE, <object>NULL
+        )
     PyArray_ENABLEFLAGS(array, NPY_ARRAY_OWNDATA)
+    if dtype.num == 20:  # void type, so most likely a UDT
+        array = array.view(dtype)
     return array
 
 
@@ -45,18 +49,20 @@ cpdef ndarray claim_buffer_2d(ffi, cdata, size_t cdata_size, size_t nrows, size_
         ndarray array
         uintptr_t ptr
         npy_intp dims[2]
+        int flags = NPY_ARRAY_WRITEABLE
     if cdata_size == size:
         ptr = int(ffi.cast("uintptr_t", cdata))
         dims[0] = nrows
         dims[1] = ncols
-        if is_c_order:
-            array = PyArray_SimpleNewFromData(2, dims, dtype.num, <void*>ptr)
-        else:
-            array = PyArray_New(
-                ndarray, 2, dims, dtype.num, NULL, <void*>ptr, -1,
-                NPY_ARRAY_F_CONTIGUOUS | NPY_ARRAY_WRITEABLE, <object>NULL
-            )
+        if not is_c_order:
+            flags |= NPY_ARRAY_F_CONTIGUOUS
+        array = PyArray_New(
+            ndarray, 2, dims, dtype.num, NULL, <void*>ptr, dtype.itemsize,
+            flags, <object>NULL
+        )
         PyArray_ENABLEFLAGS(array, NPY_ARRAY_OWNDATA)
+        if dtype.num == 20:  # void type, so most likely a UDT
+            array = array.view(dtype)
     elif cdata_size > size:  # pragma: no cover
         array = claim_buffer(ffi, cdata, cdata_size, dtype)
         if is_c_order:
