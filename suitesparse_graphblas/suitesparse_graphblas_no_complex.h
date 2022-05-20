6998b6103e393100ddc80c36e108efb8d8027f77
Erik Welch <erik.n.welch@gmail.com>
2022-05-20 18:14:28 -0500

    Update to v7.1.0 (#43)
    * Update to v7.1.0
    
    * From conda-forge in CI

--- suitesparse_graphblas/suitesparse_graphblas_no_complex.h
+++ suitesparse_graphblas/suitesparse_graphblas_no_complex.h
@@ -87,7 +87,9 @@ typedef enum
 typedef enum
 {
   GrB_NONBLOCKING = 0,
-  GrB_BLOCKING = 1
+  GrB_BLOCKING = 1,
+  GxB_NONBLOCKING_GPU = 2,
+  GxB_BLOCKING_GPU = 3
 } GrB_Mode;
 
 typedef enum
@@ -2336,6 +2338,8 @@ extern GrB_UnaryOp GxB_ATANH_FP32;
 extern GrB_UnaryOp GxB_ATANH_FP64;
 extern GrB_UnaryOp GxB_ATAN_FP32;
 extern GrB_UnaryOp GxB_ATAN_FP64;
+extern GrB_UnaryOp GxB_CBRT_FP32;
+extern GrB_UnaryOp GxB_CBRT_FP64;
 extern GrB_UnaryOp GxB_CEIL_FP32;
 extern GrB_UnaryOp GxB_CEIL_FP64;
 extern GrB_UnaryOp GxB_COSH_FP32;
@@ -3006,6 +3010,7 @@ extern GrB_Info GxB_Matrix_import_FullC(GrB_Matrix *A, GrB_Type type, GrB_Index 
 extern GrB_Info GxB_Matrix_import_FullR(GrB_Matrix *A, GrB_Type type, GrB_Index nrows, GrB_Index ncols, void **Ax, GrB_Index Ax_size, bool iso, const GrB_Descriptor desc);
 extern GrB_Info GxB_Matrix_import_HyperCSC(GrB_Matrix *A, GrB_Type type, GrB_Index nrows, GrB_Index ncols, GrB_Index **Ap, GrB_Index **Ah, GrB_Index **Ai, void **Ax, GrB_Index Ap_size, GrB_Index Ah_size, GrB_Index Ai_size, GrB_Index Ax_size, bool iso, GrB_Index nvec, bool jumbled, const GrB_Descriptor desc);
 extern GrB_Info GxB_Matrix_import_HyperCSR(GrB_Matrix *A, GrB_Type type, GrB_Index nrows, GrB_Index ncols, GrB_Index **Ap, GrB_Index **Ah, GrB_Index **Aj, void **Ax, GrB_Index Ap_size, GrB_Index Ah_size, GrB_Index Aj_size, GrB_Index Ax_size, bool iso, GrB_Index nvec, bool jumbled, const GrB_Descriptor desc);
+extern GrB_Info GxB_Matrix_isStoredElement(const GrB_Matrix A, GrB_Index i, GrB_Index j);
 extern GrB_Info GxB_Matrix_iso(bool *iso, const GrB_Matrix A);
 extern GrB_Info GxB_Matrix_memoryUsage(size_t *size, const GrB_Matrix A);
 extern GrB_Info GxB_Matrix_pack_BitmapC(GrB_Matrix A, int8_t **Ab, void **Ax, GrB_Index Ab_size, GrB_Index Ax_size, bool iso, GrB_Index nvals, const GrB_Descriptor desc);
@@ -3149,6 +3154,7 @@ extern GrB_Info GxB_Vector_fprint(GrB_Vector v, const char *name, GxB_Print_Leve
 extern GrB_Info GxB_Vector_import_Bitmap(GrB_Vector *v, GrB_Type type, GrB_Index n, int8_t **vb, void **vx, GrB_Index vb_size, GrB_Index vx_size, bool iso, GrB_Index nvals, const GrB_Descriptor desc);
 extern GrB_Info GxB_Vector_import_CSC(GrB_Vector *v, GrB_Type type, GrB_Index n, GrB_Index **vi, void **vx, GrB_Index vi_size, GrB_Index vx_size, bool iso, GrB_Index nvals, bool jumbled, const GrB_Descriptor desc);
 extern GrB_Info GxB_Vector_import_Full(GrB_Vector *v, GrB_Type type, GrB_Index n, void **vx, GrB_Index vx_size, bool iso, const GrB_Descriptor desc);
+extern GrB_Info GxB_Vector_isStoredElement(const GrB_Vector v, GrB_Index i);
 extern GrB_Info GxB_Vector_iso(bool *iso, const GrB_Vector v);
 extern GrB_Info GxB_Vector_memoryUsage(size_t *size, const GrB_Vector v);
 extern GrB_Info GxB_Vector_pack_Bitmap(GrB_Vector v, int8_t **vb, void **vx, GrB_Index vb_size, GrB_Index vx_size, bool iso, GrB_Index nvals, const GrB_Descriptor desc);
