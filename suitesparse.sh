18d0c2c785e87e0361fd71337f56d7c35485463c
Erik Welch <erik.n.welch@gmail.com>
2023-03-30 16:09:41 -0500

    consistently mark the dtypes we do/don't support

--- suitesparse.sh
+++ suitesparse.sh
@@ -52,15 +52,15 @@ cd GraphBLAS-${VERSION}/build
 # echo "#define GxB_NO_BOOL      1" >> ../Source/GB_control.h #
 # echo "#define GxB_NO_FP32      1" >> ../Source/GB_control.h #
 # echo "#define GxB_NO_FP64      1" >> ../Source/GB_control.h #
-echo "#define GxB_NO_FC32      1" >> ../Source/GB_control.h #
-echo "#define GxB_NO_FC64      1" >> ../Source/GB_control.h #
-# echo "#define GxB_NO_INT16     1" >> ../Source/GB_control.h
-# echo "#define GxB_NO_INT32     1" >> ../Source/GB_control.h
+echo "#define GxB_NO_FC32      1" >> ../Source/GB_control.h
+echo "#define GxB_NO_FC64      1" >> ../Source/GB_control.h
+# echo "#define GxB_NO_INT16     1" >> ../Source/GB_control.h #
+# echo "#define GxB_NO_INT32     1" >> ../Source/GB_control.h #
 # echo "#define GxB_NO_INT64     1" >> ../Source/GB_control.h #
-# echo "#define GxB_NO_INT8      1" >> ../Source/GB_control.h
+# echo "#define GxB_NO_INT8      1" >> ../Source/GB_control.h #
 echo "#define GxB_NO_UINT16    1" >> ../Source/GB_control.h
 echo "#define GxB_NO_UINT32    1" >> ../Source/GB_control.h
-# echo "#define GxB_NO_UINT64    1" >> ../Source/GB_control.h
+# echo "#define GxB_NO_UINT64    1" >> ../Source/GB_control.h #
 echo "#define GxB_NO_UINT8     1" >> ../Source/GB_control.h
 
 # Disable all Source/Generated2 kernels. For workflow development only.
