fac5ffe070953d1313d8755bbebb56bc3fe75fb9
Michel Pelletier <pelletier.michel@gmail.com>
2026-04-13 13:04:34 -0700

    Add links to SuiteSparse:GraphBLAS repo and User Guide in api docstring

--- suitesparse_graphblas/api/__init__.py
+++ suitesparse_graphblas/api/__init__.py
@@ -1,11 +1,12 @@
-"""SuiteSparse:GraphBLAS is a full implementation of the GraphBLAS
-standard, which defines a set of sparse matrix operations on an
-extended algebra of semirings using an almost unlimited variety of
-operators and types. When applied to sparse adjacency matrices, these
-algebraic operations are equivalent to computations on graphs.
-GraphBLAS provides a powerful and expressive framework for creating
-high-performance graph algorithms based on the elegant mathematics of
-sparse matrix operations on a semiring.
+"""[SuiteSparse:GraphBLAS](https://github.com/DrTimothyAldenDavis/GraphBLAS)
+is a full implementation of the GraphBLAS standard, which defines a
+set of sparse matrix operations on an extended algebra of semirings
+using an almost unlimited variety of operators and types. When applied
+to sparse adjacency matrices, these algebraic operations are
+equivalent to computations on graphs.  GraphBLAS provides a powerful
+and expressive framework for creating high-performance graph
+algorithms based on the elegant mathematics of sparse matrix
+operations on a semiring.
 
 This module contains the "functional API" for
 python-suitesparse-graphblas.  This is a higher level API than the
@@ -13,6 +14,11 @@ python-suitesparse-graphblas.  This is a higher level API than the
 functionality provided by the
 [python-graphblas](https://python-graphblas.readthedocs.io/en/stable/api_reference/index.html)
 library.
+
+For documentation on the SuiteSparse implementation that describes all
+of these functions in detail, read the [SuiteSparse:GraphBLAS User
+Guide](https://github.com/DrTimothyAldenDavis/GraphBLAS/blob/stable/Doc/GraphBLAS_UserGuide.pdf)
+
 """
 from suitesparse_graphblas.api import (
     binaryop,
